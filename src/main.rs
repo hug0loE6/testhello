f412658271b28f54a4e18d461e5a2c040c609e69
Hugo <hugoloe6@gmail.com>
2024-12-23 16:02:30 +0100

    3: ajout fonction et autres

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,27 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("input un nombre");
+    let lenombre = gen_alea(100);
 
-    let lenombre: i32 = rand::thread_rng().gen_range(1..101);
+    guess(lenombre, 10);
+}
 
-    
-    loop {
+fn gen_alea(rangemax: i32) -> i32 {
+    rand::thread_rng().gen_range(1..=rangemax)
+}
+
+fn guess(deviner: i32, nbessais: u8) -> bool {
+    let mut gagner = false;
+    let mut i = nbessais;
+    'essais: loop {
+        if i <= 0 {
+            println!("A court d'essais, perdu! Le nombre était : {}", deviner);
+            break 'essais;
+        }
+        println!("essais restants : {}", i);
+        println!("input un nombre : ");
         let mut entre_utilisateur = String::new();
-
-        println!("test entre : {}",entre_utilisateur);
+        
         io::stdin()
             .read_line(&mut entre_utilisateur)
             .expect("echec de l'input");
@@ -24,13 +36,16 @@ fn main() {
             }
         };
 
-        match entre_utilisateur.cmp(&lenombre) {
+        match entre_utilisateur.cmp(&deviner) {
             Ordering::Equal => {
                 println!("c bon");
-                break;
+                gagner = true;
+                break 'essais;
             }
             Ordering::Greater => println!("c moins "),
             Ordering::Less => println!("c plus"),
         }
+        i -= 1;    
     }
+    gagner
 }
